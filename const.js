b52e3aae5fbef291329d2fce32bf5b2a700cfcf1
Wissanukhong <wissanu.khong@gmail.com>
2020-02-17 21:58:06 +0700

    const

---
+++ const.js
@@ -0,0 +1,7 @@
+const numBer= 15;
+console.log(numBer);
+
+const myName = {name: "wissanu", last: "Khongjandee"};
+console.log(myName);
+
+alert("Hello word");
--- let.js
+++ let.js
@@ -1,8 +1,21 @@
+//Excercise 1
 let toyota = 20;
 
-if(toyota == 20){
-    toyota = "Yellow";
-    console.log("toyota in the block scope", toyota)
+let car = "red";
+
+if(toyota === 20){
+    let car = "Yellow";
+    console.log("toyota in the block scope", car);
 }
 
-console.log("Outside block scope", toyota)
+console.log("Outside block scope", car);
+
+//excercise 2
+let redCar = "green";
+
+if(redCar === "green"){
+    let redCar = "red";
+    console.log(redCar);
+}
+
+console.log(redCar);

565646e3bc18e4eea9c07f2d505c6c607b9c9103
Wissanukhong <wissanu.khong@gmail.com>
2020-02-18 21:14:44 +0700

    condition

---
+++ condition.js
@@ -0,0 +1,23 @@
+//Assignment statement
+var a = 5;
+var b = "Hello";
+var c; //undifine คือตัวแปรที่เรายังไม่กำหนดค่าให้กับตัวแปร
+
+console.log(a, b, c);
+console.log(a + b);
+
+//condition statement
+var x = 50;
+
+if (x >= 50) {
+  x = "ค่านี้คือ 50";
+  console.log(x);
+} else {
+  x = "ค่านี้ไม่ใช้ 50";
+}
+
+//for loop
+var i = 1;
+for (var i = 1; i < 10; i++) {
+    console.log(i);
+}

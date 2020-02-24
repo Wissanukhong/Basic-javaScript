c3455d25b305dc867bc0609f06903fc01e8742ef
Wissanukhong <wissanu.khong@gmail.com>
2020-02-24 22:30:19 +0700

    problem arrow function

---
+++ arrowFunction.js
@@ -0,0 +1,8 @@
+const myName = (name,ll) => {
+    return `...... ${name + ll}`
+}
+
+console.log(myName("wissanu","kkdk"));
+
+const myLastName = lastName => lastName;
+console.log(myLastName("wiwiwiwi"));

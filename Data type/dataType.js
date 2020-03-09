30f035583001854c0a92219536f14a969dc178d2
Wissanukhong <wissanu.khong@gmail.com>
2020-03-09 07:00:21 +0700

    relearn again

---
+++ Data type/dataType.js
@@ -0,0 +1,11 @@
+var a = 1;
+var b = "2";
+var c;
+var d = null;
+var e = false;
+
+console.log("this is type of " + typeof a);
+console.log("this is type of " + typeof b);
+console.log("this is type of " + typeof c);
+console.log("this is type of " + typeof d);
+console.log("this is type of " + typeof e);

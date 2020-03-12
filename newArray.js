faf9c77ca31d812822a3ae6da07d9176438cf5b2
Wissanukhong <wissanu.khong@gmail.com>
2020-03-12 21:10:27 +0700

    new array by array

---
+++ newArray.js
@@ -0,0 +1,8 @@
+//New array
+const store = new Array (100, 2.5, "A", "B", "C5", );
+const newStore = store.values();
+console.log(newStore.next().value);
+console.log(newStore.next().value);
+console.log(newStore.next().value);
+console.log(newStore.next().value);
+console.log(newStore.next().value);

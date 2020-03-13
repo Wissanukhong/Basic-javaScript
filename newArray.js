d6091c9662ba57c54980b1915b6923b7a69475d9
Wissanukhong <wissanu.khong@gmail.com>
2020-03-13 07:01:46 +0700

    Other Array

--- newArray.js
+++ newArray.js
@@ -1,8 +1,35 @@
-//New array
-const store = new Array (100, 2.5, "A", "B", "C5", );
+/**
+ * เป็นการสร้าง Array ใหม่ โดยการดึงค่าจาก Array เก่า
+ * จากตัวอย่างจะเห็นว่า store array จะมีค่านี้อยู่ใน Array [100, 2.5, "A", "B", "C5",];
+ * ส่วน newStore Array คือค่าที่ดึง Value มาจาก Store array นั้นเอง
+ */
+
+//การเขียน Array แบบที่ 2
+const store = new Array(100, 2.5, "A", "B", "C5");
 const newStore = store.values();
 console.log(newStore.next().value);
 console.log(newStore.next().value);
 console.log(newStore.next().value);
 console.log(newStore.next().value);
 console.log(newStore.next().value);
+
+//การเขียน Array แบบที่ 1
+let cars = ["Mazda", "Toyota", "suzuki", "Honda"];
+let newCars = cars.values();
+
+console.log(newCars.next().value);
+console.log(newCars.next().value);
+console.log(newCars.next().value);
+console.log(newCars.next().value);
+
+
+//การเขียนแบบที่ 3
+let fruits = new Array();
+fruits[0] = "Orang";
+fruits[1] = "Apple";
+fruits[2] = "Banana";
+let newFruits = fruits.values();
+
+console.log(newFruits.next().value);
+
+

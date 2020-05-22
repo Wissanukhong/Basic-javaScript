ec22d9c1e2dfd95d3b7135a5907ef70325c92589
Wissanukhong <wissanu.khong@gmail.com>
2020-05-22 16:16:50 +0700

    break

--- DOM/DOM-pig-game/app.js
+++ DOM/DOM-pig-game/app.js
@@ -8,8 +8,8 @@ activePlayer = 0;
 dice = Math.floor(Math.random() * 6 ) + 1;
 console.log(dice);
 
-//select the elelment
-document.querySelector('#current-' + activePlayer).textContent = dice;
+// select the elelment
+// document.querySelector('#current-' + activePlayer).textContent = dice;
 // document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>'
 
 var x = document.querySelector('#score-0').textContent;
@@ -19,4 +19,4 @@ console.log(x)
 document.querySelector('.dice').style.display = 'none';
 
 // addEventListener is when we click this bottom what we want it to do
-doucment.querySelector('.btn-rol').addEventListener('click');
+doucment.querySelector('.btn-roll').addEventListener('click');

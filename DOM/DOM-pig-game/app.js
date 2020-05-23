ea70c53c76d298226e3c0cff95c7ac656f9569b1
Wissanukhong <wissanu.khong@gmail.com>
2020-05-23 22:51:23 +0700

    anonymous and callback function

--- DOM/DOM-pig-game/app.js
+++ DOM/DOM-pig-game/app.js
@@ -5,18 +5,28 @@ scores = [ 0,0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6 ) + 1;
-console.log(dice);
-
 // select the elelment
 // document.querySelector('#current-' + activePlayer).textContent = dice;
 // document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>'
-
-var x = document.querySelector('#score-0').textContent;
-console.log(x)
+// var x = document.querySelector('#score-0').textContent;
 
 // hide the picture for the first time
 document.querySelector('.dice').style.display = 'none';
 
 // addEventListener is when we click this bottom what we want it to do
-doucment.querySelector('.btn-roll').addEventListener('click');
+// annymous function
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    // 1.Random number
+    // 1.1 Math.floor คือ ให้ค่าภายใน function เป็นจำนวนเต็ม 
+    var dice = Math.floor(Math.random() * 6 ) + 1;
+
+    // 2.Display the result
+    // 2.1 create the array for query the class html
+    // 2.2 การจัดการกับ css ให้รูปภาพแสดงเป็น block
+    // 2.3 Random การสุ่มรูปภาพ โดยนำค่าที่ได้มาสุ่มคู่กับ function random
+    var diceDom = document.querySelector('.dice');
+    diceDom.style.display = 'block';
+    diceDom.src = 'dice-' + dice + '.png';
+
+    //3.Update the round score If the rolled number was NOT a 1
+}); 

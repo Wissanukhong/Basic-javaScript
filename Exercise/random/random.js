1aa25684ce97217483faf523096bbf435af5993d
Wissanu.khong <wissanu.khong@gmail.com>
2020-09-02 23:33:01 +0700

    Feat : random workshop

--- Exercise/random/random.js
+++ Exercise/random/random.js
@@ -5,3 +5,11 @@ console.log(random1)
 // Math random beween 1 - 10 (both included)
 let random2 = Math.floor(Math.random()* 10);
 console.log(random2)
+
+// Math random beween 1 - 100 (both included)
+let random3 = Math.floor(Math.random() * 101);
+console.log(random3)
+
+// Math random beween 1 - 100 
+let random4 = Math.floor(Math.random() * 100) + 1;
+console.log(random4)

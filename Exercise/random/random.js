3ba8581991db6565206e966814064964f70b30a1
Wissanu.khong <wissanu.khong@gmail.com>
2020-09-02 23:26:52 +0700

    Feat: random both included

--- Exercise/random/random.js
+++ Exercise/random/random.js
@@ -1,3 +1,7 @@
-let random1 = Math.random()*10;
-
+// Math random beween 1 - 10
+let random1 = Math.random() * 10;
 console.log(random1)
+
+// Math random beween 1 - 10 (both included)
+let random2 = Math.floor(Math.random()* 10);
+console.log(random2)

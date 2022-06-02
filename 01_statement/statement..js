4be4fbaa1afa600c9260eb6910bc5f0e8b0214f7
Wissanu.Khong <wissanu.khong@gmail.com>
2022-06-02 14:42:20 +0700

    change to correct grammar

--- 01_statement/statement..js
+++ 01_statement/statement..js
@@ -1,7 +1,7 @@
-// Staement คือ คำสั่งที่บอกว่า เราต้องการให้โปรแกรมนั้นทำงานอะไร หรือว่าเรียกแบบง่ายๆ ว่าชุดคำสั่งนั้นเอง โดยจะแบ่ง statement ออกเป็น 3 หัวข้อใหญ่ๆ ดังนี้
-// Assignment statement คือ สิ่งที่เรา definded ค่าลงไปที่ตัวแปรนั้น
+// Statement คือ คำสั่งที่บอกว่า เราต้องการให้โปรแกรมนั้นทำงานอะไร หรือว่าเรียกแบบง่ายๆ ว่าชุดคำสั่งนั้นเอง โดยจะแบ่ง statement ออกเป็น 3 หัวข้อใหญ่ๆ ดังนี้
+// Assignment statement คือ สิ่งที่เรา defined ค่าลงไปที่ตัวแปรนั้น
 // condition statement คือ สิ่งเราต้องการเปรียนเทียบค่า หรือว่า ต้องการให้โปรแกรมนั้นเกิดขึ้นในกรณีไหน
-// Interation statement คือ สิ่งที่เราต้องการให้ทำซ้ำ หรือที่เรียกว่า Loop นั้นเอง
+// Iterations statement คือ สิ่งที่เราต้องการให้ทำซ้ำ หรือที่เรียกว่า Loop นั้นเอง
 
 // Assignment statement example
 var number1 = 5;
@@ -34,13 +34,13 @@ switch (number1 >= number3) {
 // Ternaly statement style
 number1 >= number3
   ? console.log(
-      `number1 มีค่ามากกว่า number3 ค่าของ number1, 2 คือ ${number1}, ${number3}`
-    )
+    `number1 มีค่ามากกว่า number3 ค่าของ number1, 2 คือ ${number1}, ${number3}`
+  )
   : console.log(
-      `number1 มีค่าน้อยกว่า number3 number1, 2 คือ ${number1}, ${number3}`
-    );
+    `number1 มีค่าน้อยกว่า number3 number1, 2 คือ ${number1}, ${number3}`
+  );
 
 // Interation statement (loop)
-for (var i = 0; i < 10; i++ ) {
-    console.log(i);
+for (var i = 0; i < 10; i++) {
+  console.log(i);
 }
